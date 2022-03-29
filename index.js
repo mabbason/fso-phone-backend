918189579f480847f5a0d47819f3859e76397258
mabbason <miles.abbason@gmail.com>
2022-03-29 15:08:29 -0400

    prepping phonebook deployment

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 app.use(morgan((tokens, req, res) => {
   const baseLog = [
     tokens.method(req, res),
